93d407328261da83783cf0a3a66c417ca5af735f
KUHHAPTC <55895335+KUHHAPTC@users.noreply.github.com>
2021-10-16 21:33:28 +0300

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,25 +1,20 @@
-"""
-Алгоритм шифрования RSA, позволяющий получить открытый и закрытый ключ для выбранного количества пользователем битов,
-скорость работы до 10 секунд для чисел от 1 до 2^1024, с использованием теста Миллера-Рабина, алгоритма Евклида и модуля random.
-Алгоритм шифрования сеансового ключа из https://ru.wikipedia.org/wiki/RSA#:~:text=RSA%20(%D0%B0%D0%B1%D0%B1%D1%80%D0%B5%D0%B2%D0%B8%D0%B0%D1%82%D1%83%D1%80%D0%B0%20%D0%BE%D1%82%20%D1%84%D0%B0%D0%BC%D0%B8%D0%BB%D0%B8%D0%B9%20Rivest,%D1%88%D0%B8%D1%84%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%2C%20%D0%B8%20%D0%B4%D0%BB%D1%8F%20%D1%86%D0%B8%D1%84%D1%80%D0%BE%D0%B2%D0%BE%D0%B9%20%D0%BF%D0%BE%D0%B4%D0%BF%D0%B8%D1%81%D0%B8. 
-"""
-
-import random
 from collections import namedtuple
+import random
+import time
+from typing import List
 
-# именованный кортеж, обращение через точку
 class PublicKey(namedtuple('PublicKey', 'n e')):
+    
     __slots__ = ()
 
-    def encrypt(self, x):
-        #  Кодирование сообщения x
-        # Третий аргумент - аргумент, на который требуется произвести деление по модулю
+    def decrypt(self, x):
+        ''' Decoding message x. 3rd arg - division by modulus. '''
+        
         return pow(x, self.e, self.n)
 
-    def decrypt(self, x):
+    def encrypt(self, x):
+        ''' Encoding message x. 3rd arg - division by modulus. '''
 
-        #  Декодирование сообщения x
-        # Третий аргумент - аргумент, на который требуется произвести деление по модулю
         return pow(x, self.e, self.n)
 
 
@@ -28,84 +23,144 @@ class PrivateKey(namedtuple('PrivateKey', 'n d')):
     __slots__ = ()
 
     def decrypt(self, x):
-
-        #  Декодирование сообщения x
-        # Третий аргумент - аргумент, на который требуется произвести деление по модулю
+        ''' Decoding message x. 3rd arg - division by modulus. '''
+        
         return pow(x, self.d, self.n)
 
     def encrypt(self, x):
-        #  Кодирование сообщения x
-        # Третий аргумент - аргумент, на который требуется произвести деление по модулю
+        ''' Encoding message x. 3rd arg - division by modulus. '''
+
         return pow(x, self.d, self.n)
  
 def is_Prime(n):
-    """
-    Miller-Rabin primality test.
+    ''' Miller-Rabin primality test.
  
     A return value of False means n is certainly not prime. A return value of
     True means n is very likely a prime.
-    """
-    # Тест Миллера-Рабина, возврашает False, если n точно не простое, возвращает True, если вероятнее всего простое.
-    if n != int(n):
-        return False
-    n = int(n)
-    #Miller-Rabin test for prime
+
+    '''
+
     if n == 0 or n == 1 or n == 4 or n == 6 or n == 8 or n == 9:
         return False
  
     if n == 2 or n == 3 or n == 5 or n == 7:
         return True
+
     s = 0
-    d = n-1
+    d = n - 1
+
     while d % 2 == 0:
         d >>= 1
         s += 1
     assert(2 ** s * d == n - 1)
  
-    def trial_composite(a):
-        if pow(a, d, n) == 1:
+    def is_trial_composite(a):
+        ''' Checks whether the number a is composite. '''
+
+        x = pow(a, d, n)
+        if x == 1 or x == (n - 1):
             return False
+
         for i in range(s):
             if pow(a, 2 ** i * d, n) == n - 1:
                 return False
+
         return True  
  
-    for i in range(8):#number of trials 
-        a = random.randrange(2, n)
-        if trial_composite(a):
+    for _ in range(8): # number of trials 
+        
+        a = random.randrange(2, n - 1)
+
+        if is_trial_composite(a):
             return False
  
     return True  
 
 def are_relatively_prime(a, b):
+    ''' Checking for mutually prime numbers, numbers are mutually prime if they have no common divisor other than 1. '''
 
-    # Проверка на взаимнопростые числа, числа являются взаимнопростыми, если у них нет общего делителя, кроме 1
     for n in range(2, min(a, b) + 1):
         if a % n == b % n == 0:
             return False
+
     return True
 
 def bezout(a, b):
+    ''' Implementation of the extended Euclidean algorithm, returns x, y and gcd(a, b) for the Bezout equation: ax + by = gcd(a, b). '''
 
-    # Реализация расширенного алгоритма Евклида, возвращает x, y и НОД(a, b) для уравнения Безу: ax + by = gcd(a, b)
     x, xx, y, yy = 1, 0, 0, 1
+
     while b:
         q = a // b
         a, b = b, a % b
-        x, xx = xx, x - xx*q
-        y, yy = yy, y - yy*q
+        x, xx = xx, x - xx * q
+        y, yy = yy, y - yy * q
+
     return (x, y, a)
 
-import time
-start_time = time.time()
+def get_q(length, p):
+    ''' We choose such a q so that it is simple and q * p < (2 ^ length). '''
 
-def make_key_pair(length):
+    q_min = (1 << (length - 1)) // p + 1
+    q_max = ((1 << length) - 1) // p
 
-    n_min = 1 << (length - 1)
-    n_max = (1 << length) - 1
+    print(f'min={q_min}, max={q_max}')
+
+    q = 4
+
+    while (is_Prime(q) == False) and (q != p):
+        q = random.randint(q_min, q_max)
+    
+    return q
+
+def get_e(eiler):
+    ''' We choose random e (has to be less than eiler) that is relatively prime with the result of eiler`s functon'''
+
+    while True:
+        e = random.randint(7, 2154)
+        if is_Prime(e) == True:
+            if are_relatively_prime(e, eiler):
+                break
+
+    return e
+
+def make_key_pair(length, primes: List[int]):
+    ''' Return a pair of keys (public and private)'''
+
+    p = random.choice(primes)
+    primes.remove(p)
+
+    q = get_q(length, p)
+
+    print(f'p={p}, q={q}')
+
+    eiler = (p - 1) * (q - 1)
+    n = p * q
+
+    e = get_e(eiler)
+
+    # The extended Euler algorithm can return a negative value, here it, in this case, is transformed into a positive one
+    gcd = bezout(e, eiler)
+    d = gcd[0] if gcd[0] > 0 else gcd[0] + eiler
+    
+    return PublicKey(n, e), PrivateKey(n, d)
+
+def time_decorator(func):
+
+    def wrapper(length):
+        time_ = time.monotonic()
+        x = func(length)
+        print(f'It took: {time.monotonic() - time_}')
+        return x
+
+    return wrapper
+
+def get_list_of_primes(length):
+    ''' Returns 10 primes from such a range that they will be > 2 ^ ([length // 2] - 1) and < than 2 ^ ([length // 2] + 1). '''
+
     start = 1 << (length // 2 - 1)
     stop = 1 << (length // 2 + 1)
-
+    print(f'start={start}, stop={stop}')
 
     primes = []
     x = random.randint(start, stop)
@@ -115,61 +170,60 @@ def make_key_pair(length):
             if x not in primes:
                 primes.append(x)
 
+    return primes
+
+def create_keys(name_of_user, length):
+    ''' Creating a public and secret key for the specified user. '''
+
+    primes = get_list_of_primes(length)
+    public, private = make_key_pair(length, primes)
+    print(f'{name_of_user}`s {public}')
+    print(f'{name_of_user}`s {private}\n')
+    
+    return public, private
 
 
+@time_decorator
+def initiate(length):
 
-    p = random.choice(primes)
-    primes.remove(p)
-    n_min1 = n_min // p + 1
-    n_max1 = n_max // p
-    q = 4
-    while (is_Prime(q) == False) and (q != p):
-        q = random.randint(n_min1, n_max1)
+    message = int(input('Input a value to encrypt: '))
 
+    public_key_receiver, private_key_receiver = create_keys('Receiver', length)
 
-    # Выбираем e, меньше чем ((p - 1) * (q - 1) - функция Эйлера)
-    # И чтобы e не имело общих множителей с ((p - 1) * (q - 1) - функция Эйлера)
-    # Слишком малые значения e ослабят безопасность шифрования RSA, поэтому начинаем с 7
-    eiler = (p - 1) * (q - 1)
-    n = p * q
-    while True:
-        e = random.randint(3, 1001)
-        if is_Prime(e) == True:
-            if are_relatively_prime(e, eiler):
-                break
-    #Расширенный алгоритм Эйлера может вернуть отрицательное значение, здесь оно, в таком случае, преображается в положительное
-    w = bezout(e, eiler)
-    if(w[0] < 0):
-        d = w[0] + eiler
-    else:
-        d = w[0]
+    public_key_sender, private_key_sender = create_keys('Sender', length)
 
-    return PublicKey(n, e), PrivateKey(n, d)
+    # Шифровка подписи закрытым ключом отправителя
+    signature = private_key_sender.encrypt(message)
 
-length = 100
-print('Input a value to encrypt: ', end = '')
-message = int(input())
-message = message % (length - 2)
-message = random.randint(2 ** message, 2 ** (length - 1))
-#Создание секретного и открытого ключей получателя
-publicRes, privateRes = make_key_pair(length)
-print('Receiver`s ' + str(publicRes))
-print('Receiver`s ' + str(privateRes))
-#Создание секретного и открытого ключей отправителя
-publicSend, privateSend = make_key_pair(length)
-print('Sender`s ' + str(publicSend))
-print('Sender`s ' + str(privateSend))
-# Шифровка пробного сообщения закрытым ключом отправителя
-autification = privateSend.encrypt(message)
-# Расшифровка пробного сообщения открытым ключом отправителя
-result = publicSend.decrypt(autification)
-print(str(autification) + " encrypted by prog Check")
-print(str(result) + " decrypted by prog Check")
+    encrypted_message = public_key_receiver.encrypt(message)
 
-#x = random.randint(2 ** (length - 1), 2 ** length)
-y = publicRes.encrypt(message)
+    encrypted_signature = public_key_receiver.encrypt(signature)
 
-print(str(y) + " encrypted by prog")
-print(str(privateRes.decrypt(y)) + " decrypted by prog")
-assert privateRes.decrypt(y) == result
-print("--- %s seconds ---" % (time.time() - start_time))
+    decrypted_message = private_key_receiver.decrypt(encrypted_message)
+
+    decrypted_signature = private_key_receiver.decrypt(encrypted_signature)
+
+    prototype = public_key_sender.decrypt(decrypted_signature)
+
+    print(f'{signature} signature that we get with private sender`s key\n')
+    print(f'{encrypted_signature} encrypted signature with public receiver`s key')
+    print(f'{decrypted_signature} decrypted signature with public receiver`s key\n')
+
+    print(f'{encrypted_message} encrypted message with public receiver`s key')
+    print(f'{decrypted_message} decrypted message with private receiver`s key')
+    print(f'{prototype} decrypted message with public sender`s key')
+    
+    return decrypted_message == prototype
+
+if __name__ == '__main__':
+    ''' The RSA encryption algorithm, which allows you to obtain a public and private key for a user-selected number of bits, a
+        speed of up to 10 seconds for numbers from 1 to 2^ 1024, using the Miller-Rabin test, the Euclid algorithm and the random 
+        module. Session key encryption algorithm from
+
+        '''
+
+    length = 100
+    result = initiate(length)
+
+    answer = 'Success' if result else 'Fail'
+    print(answer)
